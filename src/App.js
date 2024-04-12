e0ecebfb455dd5ec7ba3c084aed2b13ea3e397a6
root <root@LAPTOP-TBVPRB64>
2024-04-12 17:40:09 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Fucking React
         </a>
       </header>
     </div>
